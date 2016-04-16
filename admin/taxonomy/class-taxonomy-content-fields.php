8307004a0ae052c6a2d542f3bd5757bd4f85841a
gelther <lpw.sormano@gmail.com>
2016-04-16 14:49:33 +0200

    Code consistency changes for admin/taxonomy/class-taxonomy-content-fields.php

--- admin/taxonomy/class-taxonomy-content-fields.php
+++ admin/taxonomy/class-taxonomy-content-fields.php
@@ -15,12 +15,12 @@ class WPSEO_Taxonomy_Content_Fields extends WPSEO_Taxonomy_Fields {
 	 */
 	public function get() {
 		$fields = array(
-			'snippet' => $this->get_field_config(
+			'snippet'  => $this->get_field_config(
 				__( 'Snippet Editor', 'wordpress-seo' ),
 				sprintf( __( 'This is a rendering of what this post might look like in Google\'s search results.<br/><br/>Read %sthis post%s for more info.', 'wordpress-seo' ), '<a href="https://yoast.com/snippet-preview/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=snippet-preview">', '</a>' ),
 				'div'
 			),
-			'focuskw' => $this->get_field_config(
+			'focuskw'  => $this->get_field_config(
 				__( 'Focus Keyword', 'wordpress-seo' ),
 				sprintf( __( 'Pick the main keyword or keyphrase that this post/page is about.<br/><br/>Read %sthis post%s for more info.', 'wordpress-seo' ), '<a href="https://yoast.com/focus-keyword/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=focus-keyword">', '</a>' )
 			),
@@ -29,19 +29,19 @@ class WPSEO_Taxonomy_Content_Fields extends WPSEO_Taxonomy_Fields {
 				sprintf( __( 'This is the content analysis, a collection of content checks that analyze the content of your page. Read %sthis post%s for more info.', 'wordpress-seo' ), '<a href="https://yoast.com/real-time-content-analysis/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=snippet-preview">', '</a>' ),
 				'div'
 			),
-			'title' => $this->get_field_config(
+			'title'    => $this->get_field_config(
 				'',
 				'',
 				'hidden',
 				''
 			),
-			'desc' => $this->get_field_config(
+			'desc'     => $this->get_field_config(
 				'',
 				'',
 				'hidden',
 				''
 			),
-			'linkdex' => $this->get_field_config(
+			'linkdex'  => $this->get_field_config(
 				'',
 				'',
 				'hidden',
@@ -51,4 +51,5 @@ class WPSEO_Taxonomy_Content_Fields extends WPSEO_Taxonomy_Fields {
 
 		return $this->filter_hidden_fields( $fields );
 	}
+
 }
